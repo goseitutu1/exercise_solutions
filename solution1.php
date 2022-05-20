a076d2816e37fdf75a44d3bc195bb7c49ba204a0
goseitutu1 <oseitutugideon1@gmail.com>
2022-05-20 09:44:30 +0000

    solution one

--- solution1.php
+++ solution1.php
@@ -12,18 +12,23 @@ class Config
         return $this->values;
     }
 
+
     public function setValues($key, $element) {
-        $this->$values[$key] = $element;
+
+        $this->values[$key] = $element;
     }
 }
 
 $config = new Config();
 
-$config->getValues()['second'] = 'mango';
+//$config->getValues()['second'] = 'mango'; this is not a setter method
 
-$config->setValues('second','mango');
+$config->setValues('second','mango'); // I defined a setter method to add element to the array
 
 
 echo $config->getValues()['first'] . PHP_EOL;
 echo $config->getValues()['second']. PHP_EOL;
-echo $config->getValues()['third']. PHP_EOL;
+
+//echo $config->values['third']. PHP_EOL; the array values is a private variable, hence it can not be used outside the config class
+
+echo $config->getValues()['third']. PHP_EOL; //i used the getValues() method to get the third element
